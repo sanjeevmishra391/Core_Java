d2896aa558a9bdb336cd804bbc794d15863bacec
sanjeevmishra391 <sanjeevmishra391@gmail.com>
2023-01-31 09:54:06 +0530

    Class 16 code

--- src/Class15_4.java
+++ src/Class15_4.java
@@ -1,10 +1,11 @@
 // String class
 
 // https://www.geeksforgeeks.org/difference-between-and-equals-method-in-java/
+// https://www.w3schools.com/java/java_ref_string.asp
 
 public class Class15_4 {
     public static void main(String[] args) {
-        String name = "Sanjeev Mishra"; // ['S', 'a', 'n', 'j', 'e', 'e', 'v']
+        String name = "    Sanjeev Mishra Hey Hello Hi    "; // ['S', 'a', 'n', 'j', 'e', 'e', 'v']
 
         System.out.println("Character at 2nd index:  " + name.charAt(2));
 
@@ -23,13 +24,13 @@ public class Class15_4 {
         }
 
         if (name.equals(name2)) {
-            System.out.println("Are equal..");
+            System.out.println("Are equal.. (contains same data)");
         } else {
             System.out.println("Are not equal");
         }
 
         int position = name.indexOf('a');
-        System.out.println("Position of a = " + position);
+        System.out.println("Position of first a = " + position);
 
         int len = name.length();
         System.out.println("Length of the string = " + len);
@@ -38,5 +39,34 @@ public class Class15_4 {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
+
+        // Sanjeev Mishra
+        String sub = name.substring(2, 5); // nje
+        System.out.println("Substring: " + sub);
+
+        char[] characters = name.toCharArray();
+        for (int i = 0; i < characters.length; i++) {
+            characters[i] = (char) (characters[i] + 1);
+        }
+
+        System.out.println("Encrypted message: ");
+        for (int i = 0; i < characters.length; i++) {
+            System.out.print(characters[i]);
+        }
+
+        System.out.println("\nDecrypted message: ");
+        for (int i = 0; i < characters.length; i++) {
+            characters[i] = (char) (characters[i] - 1);
+            System.out.print(characters[i]);
+        }
+
+        String lower = name.toLowerCase();
+        System.out.println("\nLower case string: " + lower);
+
+        String upper = name.toUpperCase();
+        System.out.println("Upper case string: " + upper);
+
+        String trimmed = name.trim();
+        System.out.println("Trimmed string: " + trimmed);
     }
 }
---
+++ src/Class16.java
@@ -0,0 +1,49 @@
+// exception handling
+
+// an exception is an abnormal condition that arises in code at run time.
+// runtime-error
+
+// throw, throws, try, catch, finally
+
+// Error: Error refers to an illegal operation performed by the user which results in the abnormal working of the program.
+// Error vs Exception
+
+// When you have multiple catch statements, it it important to place exception subclasses
+// before any of their superclasses.
+
+public class Class16 {
+
+    static void division(int a, int b) {
+        System.out.println("Before try catch block");
+
+        // int n = a / 0;
+
+        try {
+            // block of code that monitors for errors(exceptions)
+            System.out.println("Beginning of try block");
+
+            int ans = a / b;
+            System.out.println("Below division");
+
+            int arr[] = new int[5];
+            arr[9] = 15;
+
+            System.out.println("At the end of try block");
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+            System.out.println("Exception: Division by zero is not possible");
+        } catch (Exception e) {
+            System.out.println("In the exception catch block");
+        }
+
+        System.out.println("Outside the try block");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("In the main method");
+
+        division(8, 0);
+
+        System.out.println("At the end of main method");
+    }
+}
